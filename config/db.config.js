1824086d1214431a1f79f781ee5a703f88fcca36
Metodi Obetsanov <htc.sens1@gmail.com>
2020-02-18 20:58:21 +0200

    Minor changes

--- config/db.config.js
+++ config/db.config.js
@@ -6,10 +6,10 @@ async function ConnectDB() {
   let conStr = null;
 
   if (process.env.NODE_ENV === 'production') {
-    logger.info('DB Status: will connect to Production DB');
+    logger.info('DB Status: Connecting to Production DB');
     conStr = config.get('production').connectionString;
   } else {
-    logger.info('DB Status: will connect to Development DB');
+    logger.info('DB Status: Connecting to Development DB');
     conStr = config.get('development').connectionString;
   }
 
