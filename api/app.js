744b0c3078a1328eb12d5dfc564af18a448ad335
Metodi Obetsanov <htc.sens1@gmail.com>
2020-02-18 10:14:37 +0200

    Uplaod
    Some minor changes

--- api/app.js
+++ api/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const passport = require('passport');
 const morgan = require('morgan');
+const Airbrake = require('@airbrake/node');
+const airbrakeExpress = require('@airbrake/node/dist/instrumentation/express');
 const swaggerUi = require('swagger-ui-express');
 const logger = require('../config/logger.config');
 const specs = require('../config/swagger.config');
 const authRouter = require('./routes/auth.route');
+
+// Configure AirBrake
+const airbrake = new Airbrake.Notifier({
+  projectId: 260107,
+  projectKey: 'd39a346d537963bbe6bf19e49af5d0d9',
+});
 
 // Init Express
 const app = express();
@@ -16,6 +24,7 @@ require('../config/passport.config')(passport);
 // Init Middleware
 app.use(express.json());
 app.use(morgan('combined', { stream: logger.stream }));
+app.use(airbrakeExpress.makeMiddleware(airbrake));
 
 // Define Routes
 app.use(
@@ -25,6 +34,7 @@ app.use(
 );
 app.use('/api/auth', authRouter);
 
+app.use(airbrakeExpress.makeErrorHandler(airbrake));
 app.use((err, req, res, next) => {
   if (err) {
     logger.error(err);
